3a18a76b19cd090f4e1202f9deb38286cb47e501
sulzkras <sulzkras@yandex.ru>
2022-08-31 08:18:23 +0700

    работа в классе

---
+++ funtion.py
@@ -0,0 +1,58 @@
+# import math
+# print(math.pi)
+# from math import pi, sin, cos
+# print(pi)
+# pi = 3.14
+# print(pi)
+
+# def f(x, y):
+# 	return  x * y
+# print(f(2, 3))
+# print(f("str", 4))
+
+# def f():
+# 	return "\"Don't let the noise of other's opinions\n\t drown out your own inner voice.\" \n\t\t\t\t\t\tSteve Jobs"
+# print(f())
+
+
+# def f(x, y, z = 3):
+# 	return  x * y + z
+# print(f(2, 3))
+# # print(f("str", 4))
+
+# def f(**kwargs):
+# 	return kwargs
+# print(f(a = 2, b = 3, c = 4))
+
+# import math
+# for name in dir(math):
+# 	# print(name, end="\n")
+# 	print(name)
+
+# import random
+# for name in dir(random):
+# 	# print(name, end="\n")
+# 	print(name)
+
+# from random import randrange, seed
+# seed(0)
+# for i in range(5):
+# 	print(randrange(2, 15, 3))
+
+# from random import randrange, seed, choice, sample
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# print(choice(lst))
+# print(sample(lst, 5))
+# print(sample(lst,10))
+#
+# import platform
+# print(platform.platform())
+# print(platform.machine())
+# print(platform.processor())
+# # print(platform.system())
+# print(platform.version())
+# print(platform.python_implementation())
+# print(platform.python_version_tuple())
+#
+#
+#
---
+++ moduli.py
@@ -0,0 +1,76 @@
+from random import randrange
+def ncht(x,y):
+    lst1 = gor*simb
+    return list1
+    lst2 = ver*simb
+    return lst2
+gor = input("Введите длинну по горизонтали: ")
+ver = input("Введите длинну по вертикали: ")
+simb = input("Введите символ: ")
+
+# result = ncht(num1, num2)
+print(lst1, end="")
+print(lst2)
+
+
+# def ncht(x,y):
+#     lst = [i for i in range(x,y+1) if not i % 2 == 0]
+#     return lst
+# num1 = int(input("Введите первое число: "))
+# num2 = int(input("Введите второе число: "))
+# result = ncht(num1, num2)
+# print(result)
+
+# import math
+# print(math.pi)
+# from math import pi, sin, cos
+# print(pi)
+# pi = 3.14
+# print(pi)
+
+# def f(x, y):
+# 	return  x * y
+# print(f(2, 3))
+# print(f("str", 4))
+
+# def f(x, y, z = 3):
+# 	return  x * y + z
+# print(f(2, 3))
+# # print(f("str", 4))
+
+# def f(**kwargs):
+# 	return kwargs
+# print(f(a = 2, b = 3, c = 4))
+
+# import math
+# for name in dir(math):
+# 	# print(name, end="\n")
+# 	print(name)
+
+# import random
+# for name in dir(random):
+# 	# print(name, end="\n")
+# 	print(name)
+
+# from random import randrange, seed
+# seed(0)
+# for i in range(5):
+# 	print(randrange(2, 15, 3))
+
+# from random import randrange, seed, choice, sample
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# print(choice(lst))
+# print(sample(lst, 5))
+# print(sample(lst,10))
+#
+# import platform
+# print(platform.platform())
+# print(platform.machine())
+# print(platform.processor())
+# print(platform.system())
+# print(platform.version())
+# print(platform.python_implementation())
+# print(platform.python_version_tuple())
+
+	
+	
